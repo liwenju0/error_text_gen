e23f64cb438087bcd07fcc6c0bed0a46e0b5ebd4
bo.zhang <bo.zhang@deepctrl.com>
2023-01-04 15:42:03 +0800

    完成pipeline

--- error_text_gen/pipeline.py
+++ error_text_gen/pipeline.py
@@ -4,6 +4,7 @@
 from .confusion import PinyinCharConfusion, PinyinWordConfusion, StrokeCharConfusion
 from .hmm import HMM
 from .pinyinapi import PinyinInputApi
+from .add_delete_char import CharAddDelete
 from  LAC import LAC
 import pycorrector as pc 
 from cache import LruCache
@@ -16,6 +17,7 @@ class GenErrorText:
         self.pinyinword_confusion = PinyinWordConfusion()
         self.strokechar_confusion = StrokeCharConfusion()
         self.pinyinapi = PinyinInputApi()
+        self.charadddelete = CharAddDelete()
         self.hmm = HMM()
         self.lac = LAC()
         self.choices = [ 
@@ -25,11 +27,13 @@ class GenErrorText:
             self.hmm,                   #隐马全拼替换占20%
             self.strokechar_confusion,  #字形替换占10%
             self.pinyinword_confusion,  #同音词替换占10%
-            self.pinyinapi,             #拼音api替换占30%
-            self.pinyinapi,
+            self.charadddelete,         #添加删除交换占10%
+            self.pinyinapi,             #拼音api替换占20%
             self.pinyinapi
 
         ]
+
+    
 
     @LruCache(maxsize=200, timeout=100)
     def forbidden_pos(self, text):
@@ -54,27 +58,49 @@ class GenErrorText:
             cur += len(w)
 
     
-    def select_pos(self, text, cnt=1):
+    def select_pos(self, text):
         poses = list(range(len(text)))
         forbidden = self.forbidden_pos(text)
         poses = list(set(poses)-set(forbidden))
-        #如果需要选取的位置数大于可选位置数的十分之一，则缩小为十分之一，最少是1
-        if cnt >= 0.1*len(poses):
-            cnt = max(int(0.1*len(poses)), 1)
-        
-        res = []
-        trycnt = 0
-        while len(res) != cnt and trycnt < 5*cnt:
-            trycnt += 1
-            select = random.choice(poses)
-            # 这里的逻辑确保至少选择一个位置
-            if select not in res:
-                res.append(select)
+        select = random.choice(poses)
+        return select  
 
-        return res  
+    def select_noiser(self):
+        return random.choice(self.choices)
+
+
+    def seg_text(self, text, stride=15):
+        words, _ = self.lac.run(text)
+        res = []
+        cur_index = 0
+        while cur_index < len(words):
+            res.append("".join(words[cur_index: cur_index+stride]))
+            cur_index += stride
+            
+
+    def generate_block(self, block):
+        pos = self.select_pos(block)
+        noiser = self.select_noiser()
+        res = noiser.error_text(block, pos=pos)
+        return res 
+        
+
 
     def generate(self, text):
+        blocks = self.seg_text(text)
+        res = []
+        for block in blocks:
+            err = self.generate_block(block)
+            res.append(err)
+        return "".join(res)
+
         
+
+
+if __name__ == "__main__":
+    tests = [
+        ""
+    ]
 
 
 
